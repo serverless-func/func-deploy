8cd2bd55b0363b8f90455082f59e9121d9b8544a
dongfg <mail@dongfg.com>
2023-11-10 10:28:00 +0800

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/serverless-aliyun/fun-deploy
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "func-deploy")
+	})
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprintf(w, "pong")
+	})
+
+	port := os.Getenv("FC_SERVER_PORT")
+	if port == "" {
+		port = "9000"
+	}
+
+	log.Println("Listening on :" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
